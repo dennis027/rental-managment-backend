34261434d87785c543530564031417b2f077303f
dennis <machariad196@gmail.com>
2026-07-09 00:08:43 +0300

    static enabled page

--- projects/settings.py
+++ projects/settings.py
@@ -51,10 +51,7 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    "apps.accounts",
-    "apps.contact",
-    "apps.analytics",
-    "apps.portfolio",
+    "apps"
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
