827f0913f21fc5c835665234163f8185ea871035
dennis <machariad196@gmail.com>
2026-07-09 14:17:24 +0300

    admin permision for models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import (
+    CustomUser,
+    Property,
+    Unit,
+    Customer,
+    RentalContract,
+    Receipt,
+    Payment,
+    Expense,
+    MaintenanceRequest,
+    SystemParameter,
+)
 
-# Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Property)
+admin.site.register(Unit)
+admin.site.register(Customer)
+admin.site.register(RentalContract)
+admin.site.register(Receipt)
+admin.site.register(Payment)
+admin.site.register(Expense)
+admin.site.register(MaintenanceRequest)
+admin.site.register(SystemParameter)
